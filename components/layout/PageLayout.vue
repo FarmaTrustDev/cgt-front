8fd1da620623e8e7e55e6a8616362fa35c4047e1
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2021-08-27 22:05:34 +0500

    fix: adding removed fiel

---
+++ components/layout/PageLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <a-card :loading="loading" :title="title">
+    <span slot="extra">
+      <a-button v-if="create" type="primary" @click="gotoState('create')"
+        >Create</a-button
+      >
+      <a-button type="primary" @click="goBack()">Back</a-button>
+      <slot name="extra"></slot>
+    </span>
+    <slot name="content" />
+  </a-card>
+</template>
+
+<script>
+import routeHelpers from '~/mixins/route-helpers'
+export default {
+  mixins: [routeHelpers],
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    create: { type: Boolean, default: true },
+  },
+  methods: {
+    gotoState(id, query = {}) {
+      this.goto(`${this.$route.path}/${id}`, query)
+    },
+  },
+}
+</script>
---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,81 @@
+<template>
+  <a-layout-sider
+    v-model="collapsed"
+    class="sidebar"
+    width="300"
+    theme="light"
+    collapsible
+  >
+    <div class="logo-container">
+      <nuxt-link to="/">
+        <img
+          src="http://cgt-dev-ft.microsysx.com/images/v2/logo.svg"
+          class="logo"
+      /></nuxt-link>
+    </div>
+    <a-skeleton :loading="isEmpty(user)" :paragraph="{ rows: 10 }">
+      <a-menu :default-selected-keys="['1']" mode="inline">
+        <a-menu-item
+          v-for="menu in user.menus"
+          :key="menu.key"
+          @click="goto(menu.to)"
+        >
+          <div class="menu-container">
+            <img :src="menu.icon" /> <span class="title">{{ menu.name }}</span>
+          </div>
+        </a-menu-item>
+        <a-menu-item key="10" @click="logout">
+          <div class="menu-container">
+            <a-icon type="logout" /> <span class="title">Logout</span>
+          </div>
+        </a-menu-item>
+      </a-menu>
+    </a-skeleton>
+  </a-layout-sider>
+</template>
+<script>
+// import { mapGetters } from 'vuex'
+import { setAccessToken, setRefreshToken } from '~/services/Auth'
+import { isEmpty } from '~/services/Helpers'
+import AuthServices from '~/services/API/AuthServices'
+export default {
+  data() {
+    return {
+      collapsed: false,
+    }
+  },
+  computed: {
+    // ...mapGetters(['getUser']),
+    user() {
+      return this.$store.getters.getUser
+    },
+  },
+  mounted() {
+    const bus = AuthServices.getBusEvent()
+    const logout = this.logout
+    bus.$on('UNAUTHORIZE', function (data) {
+      logout()
+    })
+    this.showCollapse()
+  },
+
+  methods: {
+    showCollapse() {
+      if (window.innerWidth < 960) {
+        this.collapsed = true
+      }
+    },
+    isEmpty,
+    goto(url) {
+      this.$router.push({ path: url })
+    },
+    logout() {
+      setRefreshToken('')
+      setAccessToken('')
+      this.$store.commit('setToken', {})
+      this.$store.commit('setUser', {})
+      this.$router.push({ path: '/' })
+    },
+  },
+}
+</script>
