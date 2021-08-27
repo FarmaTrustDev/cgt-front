30ca6a728c844c5da1e36ca2140e189efb2c439f
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2021-08-27 21:59:58 +0500

    fix: file transfers

---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <a-layout id="components-layout-demo-responsive">
+    <sidebar />
+    <a-layout class="main-section">
+      <a-layout-header>
+        <div class="user-detail">
+          <div>
+            Welcome <strong>{{ user.userName }}</strong>
+          </div>
+        </div>
+
+        <div class="hospital-detail">
+          <div class="capsule">
+            <img
+              src="http://sch-dev-atmps.microsysx.com/Uploads/logos/hospital/University-Hospitals-Birmingham/2020_10_22.14_48_51.jpg"
+              class="logo"
+            />
+            <span class="title">{{ user.hospitalName }}</span>
+          </div>
+        </div>
+      </a-layout-header>
+      <a-layout-content style="margin: 0 16px">
+        <!-- <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>User</a-breadcrumb-item>
+          <a-breadcrumb-item>Bill</a-breadcrumb-item>
+        </a-breadcrumb> -->
+        <div class="main">
+          <Nuxt />
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        Scheduling ©2021 Created by ATMPS
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script>
+import sidebar from '~/components/layout/Sidebar'
+
+export default {
+  components: {
+    sidebar,
+  },
+  computed: {
+    // ...mapGetters(['getUser']),
+    user() {
+      return this.$store.getters.getUser
+    },
+  },
+}
+</script>
+<style>
+#dashboard-layout .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+</style>
---
+++ services/API/AuthServices.js
@@ -0,0 +1,59 @@
+import { request, getAuthEvent } from '../request'
+import { BASE_URL, BASE_API } from '../Constant'
+
+const baseUrl = `${BASE_URL}`
+const baseApi = `${BASE_API}/auth`
+
+function login(data) {
+  return request({
+    url: `${baseApi}`,
+    data,
+    method: 'POST',
+  })
+}
+
+function getBusEvent() {
+  return getAuthEvent()
+}
+
+function logout(data = {}) {
+  return request({
+    url: `${baseUrl}logout`,
+    data,
+    method: 'POST',
+  })
+}
+
+function registerSms(data) {
+  return request({
+    url: `${baseApi}register-sms`,
+    data,
+    method: 'POST',
+  })
+}
+
+function forgetpassword(data) {
+  return request({
+    url: `${baseApi}password/create`,
+    data,
+    method: 'POST',
+  })
+}
+
+function resetPassword(data) {
+  return request({
+    url: `${baseApi}password/reset`,
+    data,
+    method: 'POST',
+  })
+}
+const AuthService = {
+  login,
+  logout,
+  registerSms,
+  forgetpassword,
+  resetPassword,
+  getBusEvent,
+}
+
+export default AuthService
