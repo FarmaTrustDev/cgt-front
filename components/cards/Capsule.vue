52f4a19add8eee0e9ea8ce6ed2f8fa37ecfc32a0
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2021-08-30 14:22:45 +0500

    feat: home page  capsule desiging

---
+++ components/cards/Capsule.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="capsule primary-capsule default-border-radius">
+    <span class="title">{{ title }} </span>
+    <span class="description">{{ description }}</span>
+    <a-icon type="right" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <Tutorial />
+  <div>
+    <a-row :gutter="12" class="grey-card">
+      <a-col :span="12">
+        <CardsCapsule title="Scan" description="Inbound Sample" />
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <script>
