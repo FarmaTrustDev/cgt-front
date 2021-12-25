3275cdaf5faac6a17883e3821b04f9778bbd77cc
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2021-12-25 15:55:22 +0500

    fix: adding standardtable by which single table can handle all the things

---
+++ components/common/StandardTable.vue
@@ -0,0 +1,55 @@
+<template>
+  <a-table
+    :loading="loading"
+    :columns="columns"
+    :pagination="pagination"
+    :data-source="data"
+  >
+    <span slot="action" slot-scope="text, record">
+      <a-button type="link" @click="goto(`${actionLink}/${record.globalId}`)">
+        <a-icon type="edit" />
+      </a-button>
+    </span>
+  </a-table>
+</template>
+<script>
+import routeHelpers from '~/mixins/route-helpers'
+import notifications from '~/mixins/notifications'
+
+export default {
+  mixins: [routeHelpers, notifications],
+  props: {
+    columns: { type: Array, default: () => [] },
+    // data: { type: Array, default: () => [] },
+    pagination: { type: Boolean, default: false },
+    actionLink: { type: String, default: '' },
+    apiService: { type: Object, required: true },
+    params: { type: Object, default: () => ({}) },
+  },
+  data() {
+    return {
+      data: [],
+      loading: false,
+    }
+  },
+  mounted() {
+    this.fetch()
+  },
+  methods: {
+    fetch(params = {}) {
+      this.loading = true
+      this.apiService
+        .get(this.params)
+        .then((response) => {
+          this.$emit('afterFetch', response)
+          this.data = response.data
+        })
+        .catch(this.error)
+        .finally(() => {
+          this.$emit('finally')
+          this.loading = false
+        })
+    },
+  },
+}
+</script>
---
+++ pages/manufacturer/schedules/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <page-layout :loading="loading" title="Treatments">
+    <template slot="content">
+      <standardTable
+        :columns="column"
+        :api-service="ManufacturerTreatmentServices"
+        :action-link="ActionLink"
+      />
+    </template>
+  </page-layout>
+</template>
+<script>
+import PageLayout from '~/components/layout/PageLayout'
+import standardTable from '~/components/common/StandardTable'
+import ManufacturerTreatmentServices from '~/services/API/ManufacturerTreatmentServices'
+const column = [
+  {
+    title: 'Treatment Name',
+    dataIndex: 'treatmentTypeName',
+    key: 'treatmentTypeName',
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    key: 'price',
+  },
+  {
+    title: 'Description',
+    dataIndex: 'description',
+    key: 'description',
+  },
+  {
+    title: 'Action',
+    dataIndex: 'action',
+    scopedSlots: { customRender: 'action' },
+  },
+]
+const ActionLink = '/manufacturer/schedules'
+export default {
+  components: { 'page-layout': PageLayout, standardTable },
+  data() {
+    return {
+      column,
+      loading: false,
+      ManufacturerTreatmentServices,
+      ActionLink,
+    }
+  },
+  methods: {},
+}
+</script>
