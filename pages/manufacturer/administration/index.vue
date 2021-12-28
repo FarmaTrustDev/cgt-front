b16907c50130b314dc4bfa64b8c1d60ad9f8b2e8
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2021-12-28 17:09:23 +0500

    feat: adding screening server

--- pages/manufacturer/administration/index.vue
+++ pages/manufacturer/administration/index.vue
@@ -1,3 +1,44 @@
 <template>
-  <div>Admins</div>
+  <div>
+    <a-row :gutter="16" justify="center" align="center">
+      <a-col
+        v-for="menu in navigation"
+        :key="menu.heading"
+        class="mt-15"
+        :xs="{ span: 24 }"
+        :md="{ span: 8 }"
+      >
+        <a-list size="small" bordered :data-source="menu.menus">
+          <a-list-item slot="renderItem" slot-scope="item">
+            <nuxt-link :to="item.to"> {{ item.name }}</nuxt-link>
+          </a-list-item>
+          <div slot="header" class="bg-gray-100">{{ menu.heading }}</div>
+        </a-list>
+      </a-col>
+    </a-row>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      navigation: [
+        {
+          heading: 'Screening Adminstration',
+          menus: [
+            {
+              icon: null,
+              id: 0,
+              initial: null,
+              key: '1',
+              name: 'Screening',
+              to: '/manufacturer/administration/screening',
+            },
+          ],
+        },
+      ],
+    }
+  },
+}
+</script>
