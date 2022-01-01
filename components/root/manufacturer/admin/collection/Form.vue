00293345b321b5f6b9b8d091b4681e189d255fd7
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-01-01 18:38:41 +0500

    feat: implmenting collection UI

---
+++ components/root/manufacturer/admin/collection/Form.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <a-form :form="form" :layout="formLayout" @submit="onSubmit">
+      <FormActionButton :loading="btnLoading" :is-created="isCreated" />
+    </a-form>
+  </div>
+</template>
+
+<script>
+// import TreatmentService from '~/services/API/TreatmentTypeServices'
+// import OrganizationServices from '~/services/API/OrganizationServices'
+import ScreeningTemplateServices from '~/services/API/ScreeningTemplateServices'
+
+// import withCrud from '~/mixins/with-crud'
+//  mixins: [withCrud],
+export default {
+  data() {
+    return {
+      screeningTemplate: {},
+      entityId: null,
+      isCreated: false,
+      loading: false,
+      treatmentType: {},
+      entity: {},
+      typeLoading: true,
+      hospitalLoading: true,
+      btnLoading: false,
+      form: this.$form.createForm(this, {
+        name: 'screening',
+      }),
+      hospitals: [],
+      formLayout: 'vertical',
+      apiService: ScreeningTemplateServices,
+      gotoLink: '/manufacturer/administration/screening',
+    }
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
--- pages/manufacturer/administration/collections/hospital/_id.vue
+++ pages/manufacturer/administration/collections/hospital/_id.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>test</div>
+  <page-layout
+    :create="false"
+    :loading="loading"
+    title="Hospital collection process"
+  >
+    <template slot="content"> <create /> </template>
+  </page-layout>
 </template>
+<script>
+import PageLayout from '~/components/layout/PageLayout'
+import create from '~/components/root/manufacturer/admin/collection/Form.vue'
+export default {
+  components: { 'page-layout': PageLayout, create },
+  data() {
+    return {
+      loading: false,
+    }
+  },
+}
+</script>
