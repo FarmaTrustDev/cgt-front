c95e65b1eb77dd3ac8ba195aabeacf7b251d678a
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-01-02 14:55:53 +0500

    feat: chat messages  layout

--- pages/chat/index.vue
+++ pages/chat/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>chat</div>
+  <page-layout
+    :loading="loading"
+    title="Chat"
+    :bordered="borderCard"
+    class="patient-page"
+  >
+    <template slot="content"><chat /></template>
+  </page-layout>
 </template>
+<script>
+import chat from '~/components/chat'
+import PageLayout from '~/components/layout/PageLayout'
+export default {
+  components: { chat, PageLayout },
+  data() {
+    return {
+      loading: false,
+      borderCard: false,
+    }
+  },
+}
+</script>
