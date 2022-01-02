c89e615a788a1a51fabfdc985c18762915e7196b
unknown <hnoor090@gmail.com>
2022-01-02 18:18:25 +0500

    fix: fixed the font issue on home tabs

--- pages/index.vue
+++ pages/index.vue
@@ -10,19 +10,19 @@
           <div class="home-tab">
             <a href="" >
               <img src="https://cgt-dev-ft.microsysx.com/images/v2/icons/note-board-scheduling.svg">
-              <h4 class="heading">Check  <strong class="strong ">New Patient </strong></h4>
+              <h4 class="heading-home">Check  <strong class="strong ">New Patient </strong></h4>
             </a>
           </div>
           <div class="home-tab">
             <a href="" >
               <img src="https://cgt-dev-ft.microsysx.com/images/v2/icons/enroll-new-patient.svg">
-              <h4 class="heading">Enroll  <strong class="strong ">Patient Status </strong></h4>
+              <h4 class="heading-home">Enroll  <strong class="strong ">Patient Status </strong></h4>
             </a>
           </div>
           <div class="home-tab with-arrow">
             <a href="">
               <img src="https://cgt-dev-ft.microsysx.com/images/v2/icons/Group%20644.svg">
-              <h4 class="heading">Manage  <strong class="strong ">Users </strong></h4>
+              <h4 class="heading-home">Manage  <strong class="strong ">Users </strong></h4>
             </a>
           </div>
          
