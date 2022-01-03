bacf6126940ad2ac2d94beb7bf162ecdc8af7024
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-01-03 21:01:02 +0500

    feat: collection work

---
+++ pages/manufacturer/administration/collections/manufacturer/_id.vue
@@ -0,0 +1,34 @@
+<template>
+  <page-layout
+    :create="false"
+    :loading="loading"
+    :title="treatmentName + ' Manufacturer Collection Process'"
+  >
+    <template slot="content">
+      <create
+        :collection-type="COLLECTION_TYPE.manufacturer.id"
+        @fetchTreatmentName="fetchTreatmentName"
+      />
+    </template>
+  </page-layout>
+</template>
+<script>
+import PageLayout from '~/components/layout/PageLayout'
+import create from '~/components/root/manufacturer/admin/collection/Form'
+import { COLLECTION_TYPE } from '~/services/Constant'
+export default {
+  components: { 'page-layout': PageLayout, create },
+  data() {
+    return {
+      loading: false,
+      treatmentName: '',
+      COLLECTION_TYPE,
+    }
+  },
+  methods: {
+    fetchTreatmentName(treatmentName) {
+      this.treatmentName = treatmentName
+    },
+  },
+}
+</script>
