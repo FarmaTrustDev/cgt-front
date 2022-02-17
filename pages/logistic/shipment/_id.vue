8caf537198eb4800c58c1e87869611c0156d8182
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-02-17 14:34:12 +0500

    fix: minor ui changes

---
+++ pages/logistic/shipment/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <page-layout :create="false" :loading="loading" title="Shipment Status">
+    <template slot="content">
+      <div class="grey-card enrollment-page">
+        <a-card class="default-border-radius ant-card">
+          <a-tabs :tab-position="mode">
+            <a-tab-pane :key="1" tab=" Pickup Shipment">
+              Pickup Shipment
+            </a-tab-pane>
+            <a-tab-pane :key="2" tab="Deliver Shipment"> t </a-tab-pane>
+          </a-tabs>
+        </a-card>
+      </div>
+    </template>
+  </page-layout>
+</template>
+<script>
+import PageLayout from '~/components/layout/PageLayout'
+
+export default {
+  components: { 'page-layout': PageLayout },
+  data() {
+    return {
+      loading: false,
+      mode: 'left',
+    }
+  },
+  methods: {},
+}
+</script>
