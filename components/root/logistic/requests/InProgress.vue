35eedadf5214832b0de7456019e276cdd141577a
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-02-17 17:48:27 +0500

    fix: inprogress and logistic listing

---
+++ components/root/logistic/requests/InProgress.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <a-table :loading="loading" :columns="column" :data-source="data">
+      <span slot="action" slot-scope="text, record">
+        <a-button type="primary" dashed @click="showConfirm(record, true)">
+          Accepted
+        </a-button>
+        <a-button type="danger" dashed @click="showConfirm(record, false)">
+          Rejected
+        </a-button>
+      </span>
+    </a-table>
+  </div>
+</template>
+<script>
+import SchedulingServices from '~/services/API/SchedulingServices'
+import withTableCrud from '~/mixins/with-table-crud'
+import { SCHEDULING_STATUSES } from '~/services/Constant'
+import {
+  _getPastMomentStandardFormatted,
+  _getFutureMomentStandardFormatted,
+} from '~/services/Helpers/MomentHelpers'
+const column = [
+  {
+    title: 'Patient Id',
+    dataIndex: 'patientEnrollmentId',
+    key: 'patientEnrollmentId',
+  },
+  {
+    title: 'Treatment Type',
+    dataIndex: 'treatmentType.name',
+    key: 'TreatmentName',
+  },
+  {
+    title: 'Organization',
+    dataIndex: 'hospital.name',
+    key: 'OrganizationName',
+  },
+  {
+    title: 'Collection - Delivery Date',
+    dataIndex: 'collectionDateDeliveryDate',
+    key: 'collectionDateDeliveryDate',
+  },
+
+  //   {
+  //     title: 'Action',
+  //     dataIndex: 'action',
+  //     scopedSlots: { customRender: 'action' },
+  //   },
+]
+const ActionLink = '/manufacturer/schedules'
+export default {
+  mixins: [withTableCrud],
+  data() {
+    return {
+      column,
+      loading: false,
+      data: [],
+      apiService: SchedulingServices,
+      ActionLink,
+      showResponseModal: false,
+      isAccepted: false,
+      params: {
+        statuses: [SCHEDULING_STATUSES.accepted.id, SCHEDULING_STATUSES.new.id],
+        start: _getPastMomentStandardFormatted(2, 'month'),
+        end: _getFutureMomentStandardFormatted(2, 'month'),
+        manufacturerStatus: SCHEDULING_STATUSES.accepted.id,
+      },
+      selectedRow: {},
+      confirmLoading: false,
+    }
+  },
+  methods: {},
+}
+</script>
