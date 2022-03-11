336e7643fef919a574eb5a5d16db584321c7a343
zulqrnainsial <zulqrain@microsysx.com>
2022-03-11 17:42:32 +0500

    feat: adding treatment collection handling

--- components/treatment/status/index.vue
+++ components/treatment/status/index.vue
@@ -11,11 +11,11 @@
           <scheduling :treatment="treatment" />
         </a-tab-pane>
         <a-tab-pane key="2" tab=" Collect Samples">
-          <collections  :treatment="treatment" />
+          <collections :treatment="treatment" />
         </a-tab-pane>
         <a-tab-pane key="3" tab=" Shipping Details">
-          Shipping Details</a-tab-pane
-        >
+          <treatmentCollection :showCompleteBtn="f" :treatment="treatment"
+        /></a-tab-pane>
         <a-tab-pane key="4" tab="Deliver Shipment">
           Deliver Shipment</a-tab-pane
         >
@@ -27,9 +27,10 @@
 <script>
 import scheduling from '~/components/treatment/enrollment/scheduling'
 import collections from '~/components/treatment/collections'
+import treatmentCollection from '~/components/treatment/collections/treatment'
 const DEFAULT_ACTIVE_KEY = '2'
 export default {
-  components: { scheduling, collections },
+  components: { scheduling, collections, treatmentCollection },
   props: {
     treatment: {
       type: Object,
