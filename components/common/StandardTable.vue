08e3ad34b87c6e5d34b2055d168cfcf47388d305
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-03-15 21:08:16 +0500

    feat: patient table listing

--- components/common/StandardTable.vue
+++ components/common/StandardTable.vue
@@ -41,33 +41,14 @@
             <span class="treatment-name-col">{{
               treatment.treatmentTypeName
             }}</span>
-            <a-steps :current="getCurrentStep(treatment)" size="small">
-              <a-step title="Screening" @click="stepClick(record, treatment)" />
-              <span class="sep-line sep-line-active"></span>
-              <a-step
-                title="Scheduling"
-                @click="stepClick(record, treatment)"
-              />
-              <span class="sep-line"></span>
-              <a-step
-                class="ant-steps-item-active"
-                title="Collection"
-                @click="gotoCollectionScreen(record, treatment)"
-              />
-              <a-step
-                title="Shipment"
-                @click="gotoCollectionScreen(record, treatment)"
-              />
-              <a-step
-                title="Treatment"
-                @click="gotoCollectionScreen(record, treatment)"
-              />
 
-              <span class="sep-line"></span>
+            <a-steps :current="getCurrentStep(treatment)" size="small">
               <a-step
-                title="After care"
-                @click="gotoCollectionScreen(record, treatment)"
-              />
+                v-for="phase in phases"
+                :key="phase.PhaseId"
+                :title="phase.name"
+              >
+              </a-step>
             </a-steps>
 
             <!-- <a-button
@@ -170,7 +151,7 @@
 import routeHelpers from '~/mixins/route-helpers'
 import notifications from '~/mixins/notifications'
 import { isEmpty, preventDefault } from '~/services/Helpers'
-
+import { PATIENT_TREATMENT_PHASES } from '~/services/Constant/Phases'
 export default {
   mixins: [routeHelpers, notifications],
   props: {
@@ -192,6 +173,7 @@ export default {
     return {
       data: [],
       loading: false,
+      phases: PATIENT_TREATMENT_PHASES,
     }
   },
   mounted() {
@@ -226,10 +208,11 @@ export default {
       return isEmpty(this.fetchFrom) ? this.apiService.get : this.fetchFrom
     },
     getCurrentStep(treatment) {
-      if (treatment.isSCheduled) {
-        return 1
-      }
-      return 3
+      return treatment.phaseId
+      // if (treatment.isSCheduled) {
+      //   return 1
+      // }
+      // return 3
     },
     stepClick(patient, treatment) {
       this.goto(`/hospital/patients/${patient.globalId}`, {
---
+++ services/Constant/Phases.js
@@ -0,0 +1,46 @@
+
+export const TREATMENT_PHASES = []
+export const PATIENT_TREATMENT_PHASES = [
+    {
+        id: 1,
+        name: 'Screening',
+        alias: 'SCREENING',
+        PhaseId: 2,
+        url_slug: '/hospital/patients/',
+    },
+    {
+        id: 2,
+        name: 'Scheduling',
+        alias: 'SCHEDULING',
+        PhaseId: 3,
+        url_slug: '/hospital/patients/collection/',
+    },
+    {
+        id: 3,
+        name: 'Collection',
+        alias: 'COLLECTION',
+        PhaseId: 5,
+        url_slug: '/hospital/patients/collection/',
+    },
+    {
+        id: 4,
+        name: 'Shipment',
+        alias: 'SHIPMENT',
+        PhaseId: 6,
+        url_slug: '/hospital/patients/collection/',
+    },
+    {
+        id: 5,
+        name: 'Treatment',
+        alias: 'TREATMENT_COLLECTION',
+        PhaseId: 14,
+        url_slug: '/hospital/patients/collection/',
+    },
+    {
+        id: 6,
+        name: 'After care',
+        alias: 'AFTER_CARE',
+        PhaseId: 15,
+        url_slug: '/hospital/patients/collection/',
+    },
+]
