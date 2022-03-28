a0b027b8c7d1717a96ff05c5037ad564bb59409c
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-03-28 19:53:37 +0500

    feat: support adding chat

---
+++ components/support/detail/Comment.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+    <a-row>
+      <a-col>
+        <!-- <pre> {{ data }}</pre> -->
+        <a-list :data-source="data" item-layout="horizontal" :bordered="false">
+          <a-list-item
+            slot="renderItem"
+            slot-scope="item"
+            :class="'message-' + getType(item.isOwner)"
+          >
+            <a-comment :author="item.ownerName" :content="item.content">
+              <template slot="content"
+                ><div class="message-time">
+                  {{ item.message }}
+                </div></template
+              >
+              <a-tooltip slot="datetime" :title="item.created_at">
+                <span>{{ item.created_at }}</span>
+              </a-tooltip>
+            </a-comment>
+          </a-list-item>
+        </a-list>
+      </a-col>
+    </a-row>
+    <a-form :form="form" :layout="formLayout" @submit="onSubmit">
+      <a-row>
+        <a-col>
+          <a-form-item>
+            <a-input
+              v-decorator="[
+                'message',
+                {
+                  rules: [
+                    { required: true, message: 'Empty message not allow' },
+                  ],
+                },
+              ]"
+              class="send-message-input"
+              placeholder="Type message here"
+            />
+            <a-input
+              v-decorator="[
+                messageTo,
+                {
+                  rules: [
+                    { required: true, message: 'Please input your name!' },
+                  ],
+                  initialValue: `${messageToId}`,
+                },
+              ]"
+              type="hidden"
+            />
+
+            <a-input
+              v-decorator="[
+                `Recipient_Name`,
+                {
+                  rules: [
+                    { required: true, message: 'Please input your name!' },
+                  ],
+                  initialValue: 'Paige Turner',
+                },
+              ]"
+              type="hidden"
+            />
+          </a-form-item>
+        </a-col>
+        <a-col class="text-right">
+          <a-form-item>
+            <a-button html-type="submit" :loading="submitting" type="primary">
+              Submit
+            </a-button>
+          </a-form-item>
+        </a-col>
+      </a-row>
+    </a-form>
+  </div>
+</template>
+<script>
+import moment from 'moment'
+// import ChatServices from '~/services/API/ChatServices'
+export default {
+  props: {
+    data: { type: Array, default: () => {} },
+    messageToId: { type: String, default: null, required: true },
+    messageTo: { type: String, default: `recipient_Id`, required: true },
+  },
+  data() {
+    return {
+      submitting: false,
+      value: '',
+      moment,
+      form: this.$form.createForm(this, {
+        name: 'chat',
+      }),
+      formLayout: 'vertical',
+    }
+  },
+  mounted() {},
+  methods: {
+    onSubmit(e) {
+      e.preventDefault()
+      this.form.validateFields((err, values) => {
+        // console.log(values)
+
+        if (!err) {
+          this.postMessage(values)
+          this.form.resetFields()
+        } else {
+          this.loading = false
+        }
+      })
+    },
+    postMessage(params) {
+      console.log(params)
+      //   ChatServices.create(params).then((response) => {
+      //     // console.log(response)
+      //     this.$emit('fetch', response)
+      //   })
+    },
+
+    handleChange(e) {
+      this.value = e.target.value
+    },
+    getType(isOwner) {
+      return isOwner ? 'received' : 'sent'
+    },
+  },
+}
+</script>
---
+++ pages/support/_id.vue
@@ -0,0 +1,26 @@
+<template>
+  <page-layout :loading="loading" title="Support Detail">
+    <template slot="content">
+      <div class="grey-card">
+        <a-row :gutter="56">
+          <a-col :span="12">
+            <Detail />
+          </a-col>
+          <a-col :span="12">
+            <a-card class="default-border-radius"> <Comment /> </a-card
+          ></a-col>
+        </a-row>
+      </div> </template
+  ></page-layout>
+</template>
+<script>
+import PageLayout from '~/components/layout/PageLayout'
+import Detail from '~/components/support/detail/index'
+import Comment from '~/components/support/detail/Comment'
+export default {
+  components: { PageLayout, Detail, Comment },
+  data() {
+    return { loading: false }
+  },
+}
+</script>
