085d998c88845f8ebdc3ff5fc937c9d4255f1ef2
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-03-30 20:43:37 +0500

    fix: adding all and com[pleted

--- pages/manufacturer/treatments/index.vue
+++ pages/manufacturer/treatments/index.vue
@@ -6,8 +6,8 @@
           <new-request />
         </a-tab-pane>
         <a-tab-pane key="2" tab="Pending"> <in-progress /> </a-tab-pane>
-        <a-tab-pane key="3" tab="Complete">4</a-tab-pane>
-        <a-tab-pane key="4" tab="All"> 4</a-tab-pane>
+        <a-tab-pane key="3" tab="Complete"><completed /></a-tab-pane>
+        <a-tab-pane key="4" tab="All"> <all/></a-tab-pane></a-tab-pane>
       </a-tabs>
     </template>
   </page-layout>
@@ -15,12 +15,16 @@
 <script>
 import PageLayout from '~/components/layout/PageLayout'
 import newRequests from '~/components/root/manufacturer/treatments/listing/NewRequest'
+import All from '~/components/root/manufacturer/treatments/listing/All'
+import Completed from '~/components/root/manufacturer/treatments/listing/Completed'
 import inProgress from '~/components/root/manufacturer/treatments/listing/InProgress'
 export default {
   components: {
     'page-layout': PageLayout,
     'new-request': newRequests,
     'in-progress': inProgress,
+    'all':All,
+    completed: Completed,
   },
   data() {
     return {
