7be076701c4e26925326365743303249c22a98aa
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-03-31 14:01:24 +0500

    feat: handling of aftercare warning

---
+++ components/treatment/enrollment/afterCare/Form.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h3 class="page-title">Aftercare Notes</h3>
+    <FormFields :treatment="treatment" :entity="entity"></FormFields>
+    <FormActionButton :is-created="isCreated" />
+  </div>
+</template>
+<script>
+import FormFields from '~/components/treatment/enrollment/afterCare/FormFields'
+import TreatmentAvailabilityServices from '~/services/API/TreatmentAvailabilityServices'
+export default {
+  components: { FormFields},
+  props: {
+    treatment: {
+      type: Object,
+      default: () => ({}),
+    },
+    form: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      isCreated: false,
+      loading: false,
+      entity: {},
+      manufacturerTreatment: {},
+    }
+  },
+  methods: {
+    fetchEvents(arg, callback) {
+      this.loading = true
+      TreatmentAvailabilityServices.get({
+        ...arg,
+        treatmentTypeId: this.treatment.treatmentTypeId,
+      })
+        .then((schedules) => {
+          callback(schedules.data)
+        })
+        .catch(this.error)
+        .finally(() => (this.loading = false))
+    },
+  },
+}
+</script>
