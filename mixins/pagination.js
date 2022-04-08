3360f87f789f5af0fdb654f2e966ee876fc45d90
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-04-08 13:24:46 +0500

    feat: implmenting pagination

---
+++ mixins/pagination.js
@@ -0,0 +1,32 @@
+export default {
+    data() {
+        return {
+            pagination: {
+                // current: 1,
+                pageSize: 10,
+                // total: 0,
+                // showTotal: () => `Total ${this.pagination.total}`,
+                onChange: (current, pageSize) =>
+                    this.pageSelect(current, pageSize)
+            }
+        };
+    },
+    methods: {
+        setPagination(data) {
+            const pagination = { ...this.pagination };
+            pagination.current = data.current;
+            // pagination.page = data.current;
+            pagination.pageSize = data.pageSize;
+            pagination.total = data.total;
+            this.pagination = pagination;
+        },
+        pageSelect(current, pageSize) {
+            const pagination = { ...this.pagination };
+            pagination.current = current;
+            pagination.page = current;
+            pagination.pageSize = pageSize;
+            this.pagination = pagination;
+            this.fetch();
+        }
+    }
+};
