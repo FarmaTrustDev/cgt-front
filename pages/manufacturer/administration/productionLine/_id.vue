3298536912635099ca18444a587f14fd6548791e
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-04-23 06:06:16 +0500

    fix: showing lab handling

---
+++ pages/manufacturer/administration/productionLine/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <page-layout :create="false" :loading="loading" title="Screening Group">
+    <template slot="content"> <create /> </template>
+  </page-layout>
+</template>
+<script>
+import PageLayout from '~/components/layout/PageLayout'
+import create from '~/components/root/manufacturer/screening/CreateForm'
+export default {
+  components: { 'page-layout': PageLayout, create },
+  data() {
+    return {
+      loading: false,
+    }
+  },
+}
+</script>
---
+++ pages/manufacturer/administration/productionLine/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <page-layout :loading="loading" title="Screenings">
+    <template slot="content">
+      <standardTable
+        :columns="column"
+        :api-service="LabServices"
+        :action-link="ActionLink"
+      />
+    </template>
+  </page-layout>
+</template>
+<script>
+import PageLayout from '~/components/layout/PageLayout'
+import standardTable from '~/components/common/StandardTable'
+import LabServices from '~/services/API/LabServices'
+const ActionLink = '/manufacturer/administration/screening'
+const column = [
+  {
+    title: 'Id',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Action',
+    dataIndex: 'action',
+    scopedSlots: { customRender: 'action' },
+  },
+]
+export default {
+  components: { 'page-layout': PageLayout, standardTable },
+  data() {
+    return {
+      loading: false,
+      column,
+      LabServices,
+      ActionLink,
+    }
+  },
+  methods: {},
+}
+</script>
