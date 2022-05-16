3b7431286b3839d39216804ee884642b157f06a6
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-05-16 13:27:58 +0500

    fix: showing home page menus

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,9 @@
 <template>
   <div class="home-page">
     <h1 class="title"><strong>My Dashboard</strong></h1>
-    <a-card class="grey-card" :bordered="false">
-      <a-row>
-        <!-- left side tabs -->
-        <a-col :span="9">
-          <div class="home-tab">
-            <a href="javascript:;" @click="goto(`/hospital/patients/create`)">
-              <img
-                src="https://cgt-dev-ft.microsysx.com/images/v2/icons/note-board-scheduling.svg"
-              />
-              <h4 class="heading-home">
-                Check <strong class="strong">New Patient </strong>
-              </h4>
-            </a>
-          </div>
-          <div class="home-tab">
-            <a href="javascript:;" @click="goto(`/hospital/patients`)">
-              <img
-                src="https://cgt-dev-ft.microsysx.com/images/v2/icons/enroll-new-patient.svg"
-              />
-              <h4 class="heading-home">
-                Enroll <strong class="strong">Patient Status </strong>
-              </h4>
-            </a>
-          </div>
-          <div class="home-tab with-arrow">
-            <a href="javascript:;" @click="goto(`/users`)">
-              <img
-                src="https://cgt-dev-ft.microsysx.com/images/v2/icons/Group%20644.svg"
-              />
-              <h4 class="heading-home">
-                Manage <strong class="strong">Users </strong>
-              </h4>
-            </a>
-          </div>
-        </a-col>
-        <!-- #end left side tabs -->
-        <a-col :span="2"></a-col>
-        <a-col :span="3"></a-col>
 
-        <a-col v-if="isManufacturer()" :span="9">
-          <TimeAndTreatmentList />
-        </a-col>
-        <a-col v-else-if="isHospital()" :span="9">
-          <treatment-stats />
-        </a-col>
-      </a-row>
-    </a-card>
+    <manufacturerHome v-if="isManufacturer()" />
+    <hospitalHome v-if="isHospital()" />
   </div>
 </template>
 
@@ -55,10 +11,10 @@
 // import { HubConnectionBuilder } from '@aspnet/signalr'
 import userDetail from '~/mixins/user-detail'
 import routeHelpers from '~/mixins/route-helpers'
-import treatmentStats from '~/components/statistics/treatment'
-import TimeAndTreatmentList from '~/components/statistics/TimeAndTreatmentList'
+import manufacturerHome from '~/components/root/manufacturer/home'
+import hospitalHome from '~/components/root/hospital/home'
 export default {
-  components: { treatmentStats, TimeAndTreatmentList },
+  components: { manufacturerHome, hospitalHome },
   mixins: [routeHelpers, userDetail],
   data() {
     return {}
