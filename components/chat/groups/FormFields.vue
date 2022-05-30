0182e1e80b3585c5860985ae020b2f0e40c93b4f
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-05-30 17:35:07 +0500

    feat: form field

--- components/chat/groups/FormFields.vue
+++ components/chat/groups/FormFields.vue
@@ -24,7 +24,7 @@
       ></a-col>
       <a-col :span="24">
         <a-form-item
-          label="Group Name:"
+          label="Group Image:"
           :label-col="{ span: 24 }"
           :wrapper-col="{ span: 22 }"
         >
@@ -35,6 +35,60 @@
         /></a-form-item>
       </a-col>
     </a-row>
+    <a-row class="mb-15">
+      <a-col :span="24">
+        <a-button type="primary" @click="addRow">+ Add Organizations</a-button>
+      </a-col>
+    </a-row>
+    <a-row v-for="org in organizationKeys" :key="org.id">
+      <a-col :span="11">
+        <a-form-item
+          label="Organizations Type:"
+          :label-col="{ span: 24 }"
+          :wrapper-col="{ span: 22 }"
+        >
+          <a-input
+            v-decorator="[
+              'Name',
+              {
+                rules: [
+                  {
+                    required: true,
+                    message: 'required!',
+                  },
+                ],
+              },
+            ]"
+            placeholder="Organizations Type"
+          /> </a-form-item
+      ></a-col>
+      <a-col :span="11">
+        <a-form-item
+          label="Organizations:"
+          :label-col="{ span: 24 }"
+          :wrapper-col="{ span: 22 }"
+        >
+          <a-input
+            v-decorator="[
+              'Name',
+              {
+                rules: [
+                  {
+                    required: true,
+                    message: 'required!',
+                  },
+                ],
+              },
+            ]"
+            placeholder="Organizations"
+          /> </a-form-item
+      ></a-col>
+      <a-col :span="2">
+        <a-button type="danger" @click="removeRow(org.id)"
+          >-</a-button
+        ></a-col
+      >
+    </a-row>
   </div>
 </template>
 
@@ -44,11 +98,31 @@ import { DOCUMENT_EXTENSIONS } from '~/services/Constant'
 export default {
   components: { Upload },
   data() {
-    return { allowedExtensions: DOCUMENT_EXTENSIONS }
+    return {
+      allowedExtensions: DOCUMENT_EXTENSIONS,
+      currentId: 0,
+      organizationKeys: [],
+    }
   },
   methods: {
     getImage(files) {
       console.log(files)
+    },
+    generateUuid() {
+      this.currentId = this.currentId + 1
+
+      return `uuid-${this.currentId}`
+    },
+    addRow() {
+      this.organizationKeys = [
+        ...this.organizationKeys,
+        { id: this.generateUuid() },
+      ]
+      console.log(this.organizationKeys)
+    },
+    removeRow(id) {
+      const org = this.organizationKeys
+      this.organizationKeys = org.filter((o) => o.id !== id)
     },
   },
 }
