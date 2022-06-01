201ce77ea5bc60484f1e3e00354eaff56ddff512
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-06-01 18:52:04 +0500

    feat: tile creating

---
+++ components/inventory/storage/Tile.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="img"><fridge :color="color" /></div>
+    <div class="location">Storage Suite 3, Germany - Cellfuse</div>
+    <div class="title">Fridge Atara 001</div>
+  </div>
+</template>
+<script>
+import fridge from '~/components/inventory/fridge'
+export default {
+  components: { fridge },
+  data() {
+    return {
+      color: '#1943AE',
+    }
+  },
+}
+</script>
---
+++ pages/inventory/storage/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Tile />
+  </div>
+</template>
+
+<script>
+import Tile from '~/components/inventory/storage/Tile'
+export default {
+  components: { Tile },
+}
+</script>
