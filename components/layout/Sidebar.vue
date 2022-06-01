05a7f5ddb208e416b8ddf3b8b1b68a19f4ef84d4
microsysx <mahdi@microsysx.com>
2022-06-01 21:07:32 +0500

    fix: adding_new_role_smartlab

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,84 +1,82 @@
 <template>
-  <a-layout-sider
-    v-model="collapsed"
-    class="sidebar"
-    width="265"
-    theme="light"
-  >
+<a-layout-sider v-model="collapsed" class="sidebar" width="265" theme="light">
     <div class="logo-container">
-      <nuxt-link to="/">
-        <img
-          src="http://cgt-dev-ft.microsysx.com/images/v2/logo.svg"
-          class="logo"
-      /></nuxt-link>
+        <nuxt-link to="/">
+            <img src="http://cgt-dev-ft.microsysx.com/images/v2/logo.svg" class="logo" /></nuxt-link>
     </div>
     <a-skeleton :loading="isEmpty(user)" :paragraph="{ rows: 10 }">
-      <a-menu :default-selected-keys="['1']" mode="inline">
-        <a-menu-item
-          v-for="menu in user.menus"
-          :key="menu.key"
-          @click="goto(menu.to)"
-        >
-          <div class="menu-container">
-            <img :src="menu.icon" style=" max-width: 28px; " /> <span class="title">{{ translation[menu.name] }}</span>
-          </div>
-        </a-menu-item>
-        <a-menu-item key="10" @click="logout" class="logout-menu">
-          <div class="menu-container">
-            <img :src="logoutImg"> <span class="title">{{translation.Logou_1_37}}</span>
-          </div>
-        </a-menu-item>
-      </a-menu>
+        <a-menu :default-selected-keys="['1']" mode="inline">
+            <a-menu-item v-for="menu in user.menus" :key="menu.key" @click="goto(menu.to)">
+                <div class="menu-container">
+                    <img :src="menu.icon" style=" max-width: 28px; " /> <span class="title">{{ translation[menu.name] }}</span>
+                </div>
+            </a-menu-item>
+            <a-menu-item key="10" @click="logout" class="logout-menu">
+                <div class="menu-container">
+                    <img :src="logoutImg"> <span class="title">{{translation.Logou_1_37}}</span>
+                </div>
+            </a-menu-item>
+        </a-menu>
     </a-skeleton>
-  </a-layout-sider>
+</a-layout-sider>
 </template>
+
 <script>
 // import { mapGetters } from 'vuex'
-import { setAccessToken, setRefreshToken } from '~/services/Auth'
-import { isEmpty } from '~/services/Helpers'
+import {
+    setAccessToken,
+    setRefreshToken
+} from '~/services/Auth'
+import {
+    isEmpty
+} from '~/services/Helpers'
 import AuthServices from '~/services/API/AuthServices'
 export default {
-  data() {
-    return {
-      collapsed: false,
-      logoutImg : "https://cgt-dev-ft.microsysx.com/images/v2/icons/logout.svg?0.229"
-    }
-  },
-  computed: {
-    // ...mapGetters(['getUser']),
-    user() {
-      return this.$store.getters.getUser
+    data() {
+        return {
+            collapsed: false,
+            logoutImg: "https://cgt-dev-ft.microsysx.com/images/v2/icons/logout.svg?0.229"
+        }
     },
-    translation() {
-      return this.$store.getters.getTranslation
+    computed: {
+        // ...mapGetters(['getUser']),
+        user() {
+            return this.$store.getters.getUser
+        },
+        translation() {
+            return this.$store.getters.getTranslation
+        },
     },
-  },
-  mounted() {
-    const bus = AuthServices.getBusEvent()
-    const logout = this.logout
-    bus.$on('UNAUTHORIZE', function (data) {
-      logout()
-    })
-    this.showCollapse()
-  },
+    mounted() {
+        const bus = AuthServices.getBusEvent()
+        const logout = this.logout
+        bus.$on('UNAUTHORIZE', function (data) {
+            logout()
+        })
+        this.showCollapse()
+    },
 
-  methods: {
-    showCollapse() {
-      if (window.innerWidth < 960) {
-        this.collapsed = true
-      }
+    methods: {
+        showCollapse() {
+            if (window.innerWidth < 960) {
+                this.collapsed = true
+            }
+        },
+        isEmpty,
+        goto(url) {
+            this.$router.push({
+                path: url
+            })
+        },
+        logout() {
+            setRefreshToken('')
+            setAccessToken('')
+            this.$store.commit('setToken', {})
+            this.$store.commit('setUser', {})
+            this.$router.push({
+                path: '/login'
+            })
+        },
     },
-    isEmpty,
-    goto(url) {
-      this.$router.push({ path: url })
-    },
-    logout() {
-      setRefreshToken('')
-      setAccessToken('')
-      this.$store.commit('setToken', {})
-      this.$store.commit('setUser', {})
-      this.$router.push({ path: '/login' })
-    },
-  },
 }
 </script>
---
+++ components/root/smartlab/home/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="home-page">
+    <a-card class="grey-card" :bordered="false">
+      <a-row>
+        <!-- left side tabs -->
+        <a-col :span="9">
+          <div class="home-tab">
+            <a href="javascript:;" @click="goto(`/manufacturer/schedules`)">
+              <img
+                src="https://cgt-dev-ft.microsysx.com/images/v2/icons/note-board-scheduling.svg" width="120" height="100"
+              />
+              <h4 class="heading-home">
+                {{translation.Manuf_1_342}} <strong class="strong"> {{translation.Sched_1_21}} </strong>
+              </h4>
+            </a>
+          </div>
+          <div class="home-tab">
+            <a href="javascript:;" @click="goto(`/manufacturer/treatments`)">
+              <img
+                src="https://cgt-dev-ft.microsysx.com/images/v2/icons/enroll-new-patient.svg" width="120" height="100"
+              />
+              <h4 class="heading-home">
+                Manage <strong class="strong"> {{translation.Treat_1_29}} </strong>
+              </h4>
+            </a>
+          </div>
+          <div class="home-tab with-arrow">
+            <a href="javascript:;" @click="goto(`/users`)">
+              <img
+                src="https://cgt-dev-ft.microsysx.com/images/v2/icons/Group%20644.svg" width="120" height="100"
+              />
+              <h4 class="heading-home">
+                Manage <strong class="strong"> {{translation.ManagUsers_2_17}} </strong>
+              </h4>
+            </a>
+          </div>
+        </a-col>
+        <!-- #end left side tabs -->
+        <a-col :span="2"></a-col>
+        <a-col :span="3"></a-col>
+
+        <!-- <a-col :span="9">
+          <TimeAndTreatmentList />
+        </a-col> -->
+      </a-row>
+    </a-card>
+  </div>
+</template>
+
+<script>
+// import { HubConnectionBuilder } from '@aspnet/signalr'
+import userDetail from '~/mixins/user-detail'
+import routeHelpers from '~/mixins/route-helpers'
+// import TimeAndTreatmentList from '~/components/statistics/TimeAndTreatmentList'
+export default {
+//   components: { TimeAndTreatmentList },
+  mixins: [routeHelpers, userDetail],
+  data() {
+    return {}
+  },
+  computed:{
+    translation() {
+      return this.$store.getters.getTranslation
+    },
+  },
+  mounted() {},
+  methods: {
+    gotoView(uri) {
+      this.goto(`/${uri}`)
+    },
+  },
+}
+</script>
