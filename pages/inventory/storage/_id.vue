b1a4b640a485f7763501943079a3499e3af6ec9a
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-06-02 13:19:40 +0500

    feat: adding some images

--- pages/inventory/storage/_id.vue
+++ pages/inventory/storage/_id.vue
@@ -6,21 +6,22 @@
     title="Storage Services"
   >
     <div slot="content">
-      <div class="w-800 margin-auto">
-        <a-row :gutter="16">
-          <a-col v-for="storage in storages" :key="storage.id" :span="6">
-            <Tile :storage="storage" />
-          </a-col>
-        </a-row>
-      </div>
+      <a-row>
+        <a-col :span="8"><fridge /></a-col>
+        <a-col :span="8"><Shelf /></a-col>
+        <a-col :span="8"><TestTube /></a-col>
+      </a-row>
     </div>
   </page-layout>
 </template>
 
 <script>
 import PageLayout from '~/components/layout/PageLayout'
+import fridge from '~/components/inventory/fridge'
+import Shelf from '~/components/inventory/assets/Shelf'
+import TestTube from '~/components/inventory/assets/TestTube'
 export default {
-  components: { PageLayout },
+  components: { PageLayout, Shelf, TestTube, fridge },
   data() {
     return {}
   },
