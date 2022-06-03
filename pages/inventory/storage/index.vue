15b377afcfb140951dbe1500dc4efa17652e55b6
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-06-03 18:23:40 +0500

    feat: inventory storage screening handling

--- pages/inventory/storage/index.vue
+++ pages/inventory/storage/index.vue
@@ -6,7 +6,7 @@
     title="Storage Services"
   >
     <div slot="content">
-      <div class="w-1200 p-15 default-border-radius">
+      <div class="w-1200 margin-auto p-15 default-border-radius">
         <a-row :gutter="16">
           <a-col v-for="storage in storages" :key="storage.id" :span="6">
             <span @click="goto(`/inventory/storage/1`)">
@@ -49,6 +49,66 @@ export default {
         },
         {
           id: 4,
+          color: '#1943AE',
+          title: 'Fridge Atara 001',
+          location: 'Storage Suite 3, Germany - Cellfuse',
+        },
+        {
+          id: 5,
+          color: '#FA6363',
+          title: 'Fridge Atara 001',
+          location: 'Storage Suite 3, Germany - Cellfuse',
+        },
+        {
+          id: 6,
+          color: '#1943AE',
+          title: 'Fridge Atara 001',
+          location: 'Storage Suite 3, Germany - Cellfuse',
+        },
+        {
+          id: 5,
+          color: '#FA6363',
+          title: 'Fridge Atara 001',
+          location: 'Storage Suite 3, Germany - Cellfuse',
+        },
+        {
+          id: 6,
+          color: '#1943AE',
+          title: 'Fridge Atara 001',
+          location: 'Storage Suite 3, Germany - Cellfuse',
+        },
+        {
+          id: 5,
+          color: '#FA6363',
+          title: 'Fridge Atara 001',
+          location: 'Storage Suite 3, Germany - Cellfuse',
+        },
+        {
+          id: 6,
+          color: '#1943AE',
+          title: 'Fridge Atara 001',
+          location: 'Storage Suite 3, Germany - Cellfuse',
+        },
+        {
+          id: 5,
+          color: '#FA6363',
+          title: 'Fridge Atara 001',
+          location: 'Storage Suite 3, Germany - Cellfuse',
+        },
+        {
+          id: 6,
+          color: '#1943AE',
+          title: 'Fridge Atara 001',
+          location: 'Storage Suite 3, Germany - Cellfuse',
+        },
+        {
+          id: 5,
+          color: '#FA6363',
+          title: 'Fridge Atara 001',
+          location: 'Storage Suite 3, Germany - Cellfuse',
+        },
+        {
+          id: 6,
           color: '#1943AE',
           title: 'Fridge Atara 001',
           location: 'Storage Suite 3, Germany - Cellfuse',
