9fb42ecbc65c8cc39530d321c9cc7c6ab4fa0c9d
microsysx <mahdi@microsysx.com>
2022-06-03 17:28:32 +0500

    fix: smartlab_treatment_UI_changes

--- components/root/manufacturer/treatments/listing/NewRequest.vue
+++ components/root/manufacturer/treatments/listing/NewRequest.vue
@@ -1,161 +1,154 @@
 <template>
-  <span>
+<span>
     <Filters @getParams="getParams" />
     <a-table :loading="loading" :columns="column" :data-source="data">
-      <span slot="action" slot-scope="text, record">
-        <div v-if="showButton(record)">
-          <a-button type="primary" dashed @click="showConfirm(record, true)">
-            {{translation.Accep_1_278}}
-          </a-button>
-          <a-button type="danger" dashed @click="showConfirm(record, false)">
-            {{translation.Rejec_1_280}}
-          </a-button>
-        </div>
-        <div v-else>
-          <a-badge>{{
+        <span slot="action" slot-scope="text, record">
+            <div v-if="showButton(record)">
+                <a-button type="primary" dashed @click="showConfirm(record, true)">
+                    {{translation.Accep_1_278}}
+                </a-button>
+                <a-button type="danger" dashed @click="showConfirm(record, false)">
+                    {{translation.Rejec_1_280}}
+                </a-button>
+            </div>
+            <div v-else>
+                <a-badge>{{
             record.treatment.isDead ? 'Patient Dead' : 'Treatment is on hold'
           }}</a-badge>
-        </div>
-      </span>
+            </div>
+        </span>
     </a-table>
 
-    <a-modal
-      title="Scheduling Request"
-      :visible="showResponseModal"
-      :confirm-loading="confirmLoading"
-      :footer="null"
-      :destroy-on-close="true"
-      :width="700"
-      @ok="submitTreatmentResult"
-      @cancel="handleModal(false)"
-    >
-      <a-form :form="form" :layout="formLayout" @submit="onSubmit">
-        <Form :is-accepted="isAccepted" :data="selectedRow" />
+    <a-modal title="Scheduling Request" :visible="showResponseModal" :confirm-loading="confirmLoading" :footer="null" :destroy-on-close="true" :width="700" @ok="submitTreatmentResult" @cancel="handleModal(false)">
+        <a-form :form="form" :layout="formLayout" @submit="onSubmit">
+            <Form :is-accepted="isAccepted" :data="selectedRow" />
 
-        <FormActionButton
-          :text="getButtonText()"
-          :btn-type="getButtonType()"
-          :is-created="false"
-          ><span slot="extra" class="mr-5">
-            <a-button @click="handleModal(false)">{{translation.cance_1_296}}</a-button>
-          </span></FormActionButton
-        >
-      </a-form>
+            <FormActionButton :text="getButtonText()" :btn-type="getButtonType()" :is-created="false"><span slot="extra" class="mr-5">
+                    <a-button @click="handleModal(false)">{{translation.cance_1_296}}</a-button>
+                </span></FormActionButton>
+        </a-form>
     </a-modal>
-  </span>
+</span>
 </template>
+
 <script>
 import Form from '~/components/root/manufacturer/treatments/request/Form'
 import SchedulingServices from '~/services/API/SchedulingServices'
 import Filters from '~/components/root/manufacturer/treatments/listing/Filters'
 import withTableCrud from '~/mixins/with-table-crud'
 import {
-  _getPastMomentStandardFormatted,
-  _getFutureMomentStandardFormatted,
+    _getPastMomentStandardFormatted,
+    _getFutureMomentStandardFormatted,
 } from '~/services/Helpers/MomentHelpers'
-import { SCHEDULING_STATUSES } from '~/services/Constant'
+import {
+    SCHEDULING_STATUSES
+} from '~/services/Constant'
 const ActionLink = '/manufacturer/schedules'
 export default {
-  components: { Form, Filters },
-  mixins: [withTableCrud],
-  data() {
-    return {
-      column:[
-  {
-    title: `${this.$store.getters.getTranslation.PatieID_2_264}`,
-    dataIndex: 'patientEnrollmentNumber',
-    key: 'patientEnrollmentNumber',
-  },
-  {
-    title: `${this.$store.getters.getTranslation.TreatType_2_67}`,
-    dataIndex: 'treatmentType.name',
-    key: 'TreatmentName',
-  },
-  {
-    title: `${this.$store.getters.getTranslation.Organ_1_166}`,
-    dataIndex: 'hospital.name',
-    key: 'OrganizationName',
-  },
-  {
-    title: `${this.$store.getters.getTranslation['Colle-_4_268']}`,
-    dataIndex: 'collectionDateDeliveryDate',
-    key: 'collectionDateDeliveryDate',
-  },
+    components: {
+        Form,
+        Filters
+    },
+    mixins: [withTableCrud],
+    data() {
+        return {
+            column: [{
+                    title: `${this.$store.getters.getTranslation.PatieID_2_264}`,
+                    dataIndex: 'patientEnrollmentNumber',
+                    key: 'patientEnrollmentNumber',
+                },
+                {
+                    title: `${this.$store.getters.getTranslation.TreatType_2_67}`,
+                    dataIndex: 'treatmentType.name',
+                    key: 'TreatmentName',
+                },
+                {
+                    title: `${this.$store.getters.getTranslation.Organ_1_166}`,
+                    dataIndex: 'hospital.name',
+                    key: 'OrganizationName',
+                },
+                {
+                    title: `${this.$store.getters.getTranslation['Colle-_4_268']}`,
+                    dataIndex: 'collectionDateDeliveryDate',
+                    key: 'collectionDateDeliveryDate',
+                },
 
-  {
-    title: `${this.$store.getters.getTranslation.Actio_1_220}`,
-    dataIndex: 'action',
-    scopedSlots: { customRender: 'action' },
-  },
-],
-      loading: false,
-      data: [],
-      apiService: SchedulingServices,
-      ActionLink,
-      showResponseModal: false,
-      isAccepted: false,
-      params: {
-        manufacturerStatus: SCHEDULING_STATUSES.pending.id,
-        start: _getPastMomentStandardFormatted(2, 'month'),
-        end: _getFutureMomentStandardFormatted(2, 'month'),
-        active: true,
-        direction: 1,
-      },
-      selectedRow: {},
-      confirmLoading: false,
-      form: this.$form.createForm(this, {
-        name: 'screeningCategory',
-      }),
-      formLayout: 'vertical',
-    }
-  },
-  methods: {
-    showConfirm(record, isAccepted) {
-      this.isAccepted = isAccepted
-      this.selectedRow = record
-      this.handleModal(true)
-    },
-    getButtonText() {
-      return this.isAccepted ? this.$store.getters.getTranslation.Accep_1_278 : this.$store.getters.getTranslation.Rejec_1_280
-    },
-    getButtonType() {
-      return this.isAccepted ? 'primary' : 'danger'
-    },
-    handleModal(show) {
-      if (!show) {
-        this.isAccepted = false
-      }
-      this.showResponseModal = show
-    },
-    submitTreatmentResult() {},
-    onSubmit(e) {
-      this.loading = true
-      e.preventDefault()
-      this.form.validateFields((err, values) => {
-        if (!err) {
-          const data = this.selectedRow
-          SchedulingServices.markScheduleRequest(data.id, values).then(
-            (response) => {
-              this.success(response.message)
-              this.handleModal(false)
-              this.fetch()
-            }
-          )
+                {
+                    title: `${this.$store.getters.getTranslation.Actio_1_220}`,
+                    dataIndex: 'action',
+                    scopedSlots: {
+                        customRender: 'action'
+                    },
+                },
+            ],
+            loading: false,
+            data: [],
+            apiService: SchedulingServices,
+            ActionLink,
+            showResponseModal: false,
+            isAccepted: false,
+            params: {
+                manufacturerStatus: SCHEDULING_STATUSES.pending.id,
+                start: _getPastMomentStandardFormatted(2, 'month'),
+                end: _getFutureMomentStandardFormatted(2, 'month'),
+                active: true,
+                direction: 1,
+            },
+            selectedRow: {},
+            confirmLoading: false,
+            form: this.$form.createForm(this, {
+                name: 'screeningCategory',
+            }),
+            formLayout: 'vertical',
         }
-      })
-      this.loading = false
     },
-    showButton(schedule) {
-      return !(schedule.treatment.isHold || schedule.treatment.isDead)
+    methods: {
+        showConfirm(record, isAccepted) {
+            this.isAccepted = isAccepted
+            this.selectedRow = record
+            this.handleModal(true)
+        },
+        getButtonText() {
+            return this.isAccepted ? this.$store.getters.getTranslation.Accep_1_278 : this.$store.getters.getTranslation.Rejec_1_280
+        },
+        getButtonType() {
+            return this.isAccepted ? 'primary' : 'danger'
+        },
+        handleModal(show) {
+            if (!show) {
+                this.isAccepted = false
+            }
+            this.showResponseModal = show
+        },
+        submitTreatmentResult() {},
+        onSubmit(e) {
+            this.loading = true
+            e.preventDefault()
+            this.form.validateFields((err, values) => {
+                if (!err) {
+                    const data = this.selectedRow
+                    SchedulingServices.markScheduleRequest(data.id, values).then(
+                        (response) => {
+                            this.success(response.message)
+                            this.handleModal(false)
+                            this.fetch()
+                        }
+                    )
+                }
+            })
+            this.loading = false
+        },
+        showButton(schedule) {
+            return !(schedule.treatment.isHold || schedule.treatment.isDead)
+        },
+        getParams(params) {
+            this.fetch(params)
+        },
     },
-    getParams(params) {
-      this.fetch(params)
-    },
-  },
-  computed:{
-    translation() {
-      return this.$store.getters.getTranslation
-    },
-  }
+    computed: {
+        translation() {
+            return this.$store.getters.getTranslation
+        },
+    }
 }
 </script>
---
+++ pages/smartlab/treatment.vue
@@ -0,0 +1,168 @@
+<template>
+<div>
+    <div class="page-header clearfix">
+        <h3 class="page-title pl-5 float-left">Treatment List</h3>
+        <!-- {{translation['All-_3_400']}} -->
+        <a-select class="float-right page-search-input" :placeholder="translation.TreatType_2_67">
+            <a-select-option v-for="treatmentType in treatmentTypes" :key="treatmentType.id">{{ treatmentType.name }}</a-select-option>
+        </a-select>
+    </div>
+    <div class="h-tabs large-tabs">
+        <a-tabs type="card" :animated="false">
+            <a-tab-pane key="1" :tab="translation.NewTreat_2_260">
+                <standard-table :columns="[{
+    title: `Patient ID`,
+    dataIndex: 'patientEnrollmentNumber',
+    key: 'patientEnrollmentNumber',
+  },
+   {
+    title: `Treatment Type`,
+    dataIndex: 'treatmentType',
+    key: 'treatmentType',
+  },
+  {
+    title: `Organizations`,
+    dataIndex: 'hospital',
+    key: 'hospital',
+  },
+  {
+    title: `Collection - Delivery Date`,
+    dataIndex: 'collectionDateDeliveryDate',
+    key: 'collectionDateDeliveryDate',
+  },
+  {
+                    title: `Actions`,
+                    dataIndex: 'action',
+                    scopedSlots: {
+                    customRender: 'action'
+                    },
+                },
+  ]"
+  :dump-data="[{patientEnrollmentNumber: 1, treatmentType: 'ABC', hospital: 'Agha Khan Hospital', collectionDateDeliveryDate: '12/12/2022' }]" :should-fetch="false" />
+            </a-tab-pane>
+            <a-tab-pane key="2" :tab="translation.PendiTreat_2_300">
+                <standard-table :columns="[{
+                  title: `Patient ID`,
+                  dataIndex: 'patientEnrollmentNumber',
+                  key: 'patientEnrollmentNumber',
+                },
+                {
+                  title: `Treatment Type`,
+                  dataIndex: 'treatmentName',
+                  key: 'treatmentName',
+                },
+                {
+                  title: `Production Line`,
+                  dataIndex: 'productionLine',
+                  key: 'productionLine',
+                },
+                {
+                  title: `Organizations`,
+                  dataIndex: 'hospital',
+                  key: 'hospital',
+                },
+                {
+                  title: `Collection - Delivery Date`,
+                  dataIndex: 'collectionDateDeliveryDate',
+                  key: 'collectionDateDeliveryDate',
+                },
+                {
+                  title: `Action`,
+                  dataIndex: 'action',
+                  scopedSlots: { customRender: 'action' },
+                },
+              ]" :dump-data="[{patientEnrollmentNumber: 1,treatmentName: 'abc', productionLine: 'Prod', hospital: 'CellFuse', collectionDateDeliveryDate: '12/10/2022'}]" :should-fetch="false" />
+            </a-tab-pane>
+            <a-tab-pane key="3" :tab="translation.ComplTreat_2_844">
+                <standard-table :columns="[{
+                  title: `Patient ID`,
+                  dataIndex: 'patientEnrollmentNumber',
+                  key: 'patientEnrollmentNumber',
+                },
+                {
+                  title: `Treatment Type`,
+                  dataIndex: 'treatmentType',
+                  key: 'treatmentType',
+                },
+                {
+                  title: `Organizations`,
+                  dataIndex: 'hospital',
+                  key: 'hospital',
+                },
+                {
+                  title: `Collection - Delivery Date`,
+                  dataIndex: 'collectionDateDeliveryDate',
+                  key: 'collectionDateDeliveryDate',
+                },
+              ]" :dump-data="[{patientEnrollmentNumber: 1,treatmentType: 'ABC', hospital: 'AKU', collectionDateDeliveryDate: '12/22/2022'  }]" :should-fetch="false"
+              />
+            </a-tab-pane>
+            <a-tab-pane key="4" :tab="translation['All-_3_400']">
+                <standard-table :columns="[
+  {
+    title: `Patient ID`,
+    dataIndex: 'patientEnrollmentNumber',
+    key: 'patientEnrollmentNumber',
+    scopedSlots: { customRender: 'name' },
+  },
+  {
+    title: `Treatment Type`,
+    dataIndex: 'treatmentType',
+    key: 'treatmentType',
+  },
+  {
+   title: `Organizations`,
+    dataIndex: 'hospital',
+    key: 'hospital',
+  },
+  {
+    title: `Collection - Delivery Date`,
+    dataIndex: 'collectionDateDeliveryDate',
+    key: 'collectionDateDeliveryDate',
+  },
+
+  // {
+  //   title: 'Action',
+  //   dataIndex: 'action',
+  //   scopedSlots: { customRender: 'action' },
+  // },
+]"
+:dump-data="[{patientEnrollmentNumber: 1, treatmentType: 'ABC', hospital: 'AKUH', collectionDateDeliveryDate: '12/10/2022'}]" :should-fetch="false"
+ />
+            </a-tab-pane>
+        </a-tabs>
+    </div>
+</div>
+</template>
+
+<script>
+// import newRequests from '~/components/root/manufacturer/treatments/listing/NewRequest'
+// import All from '~/components/root/manufacturer/treatments/listing/All'
+// import Completed from '~/components/root/manufacturer/treatments/listing/Completed'
+// import inProgress from '~/components/root/manufacturer/treatments/listing/InProgress'
+import StandardTable from '~/components/common/StandardTable'
+// @todo for Naveed here optimize in 1 table single component can handle the calls
+export default {
+    components: {
+        // 'new-request': newRequests,
+        // 'in-progress': inProgress,
+        // all: All,
+        // completed: Completed,
+        StandardTable,
+    },
+    data() {
+        return {
+            loading: false,
+            treatmentTypes: [],
+        }
+    },
+    computed: {
+        translation() {
+            return this.$store.getters.getTranslation
+        },
+    },
+    methods: {
+        searchTreatment() {},
+    },
+}
+</script>
