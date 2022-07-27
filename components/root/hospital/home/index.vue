754fea174f5f8c1f1052fbb54b28a8c421d467ae
Maaz Khan <maazkghori10@gmail.com>
2022-07-27 19:18:20 +0500

    fix: homepage

--- components/root/hospital/home/index.vue
+++ components/root/hospital/home/index.vue
@@ -1,44 +1,59 @@
 <template>
   <div class="home-page">
     <a-card class="grey-card" :bordered="false">
-        <!-- left side tabs -->
-        <a-col :span="12" class="mtminus-7">
+      <!-- left side tabs -->
+      <a-col :md="10" class="mtminus-7">
+        <div class="home-tab">
           <a href="javascript:;" @click="goto(`/hospital/patients/create`)">
-            <div class="home-tab">
-              <img
-                src="https://cgt-dev-ft.microsysx.com/images/v2/icons/note-board-scheduling.svg" width="120" height="100"
-              />
-              <h4 class="heading-home">
-                {{translation.Check_1_454}} <strong class="strong">{{translation['new_1_276']}} {{translation['Patie_1_184']}} </strong>
-              </h4>
-            </div>
+            <img
+              src="https://cgt-dev-ft.microsysx.com/images/v2/icons/note-board-scheduling.svg"
+              width="120"
+              height="100"
+            />
+            <h4 class="heading-home">
+              {{ translation.Check_1_454 }}
+              <strong class="strong"
+                >{{ translation['new_1_276'] }} {{ translation['Patie_1_184'] }}
+              </strong>
+            </h4>
           </a>
+        </div>
+        <div class="home-tab">
           <a href="javascript:;" @click="goto(`/hospital/patients`)">
-            <div class="home-tab">
-              <img
-                src="https://cgt-dev-ft.microsysx.com/images/v2/icons/enroll-new-patient.svg" width="120" height="100"
-              />
-              <h4 class="heading-home">
-                {{translation.enrol_1_496}} <strong class="strong">{{translation['Patie_1_184']}} {{translation['Statu_1_202']}} </strong>
-              </h4>
-            </div>
+            <img
+              src="https://cgt-dev-ft.microsysx.com/images/v2/icons/enroll-new-patient.svg"
+              width="120"
+              height="100"
+            />
+            <h4 class="heading-home">
+              {{ translation.enrol_1_496 }}
+              <strong class="strong"
+                >{{ translation['Patie_1_184'] }}
+                {{ translation['Statu_1_202'] }}
+              </strong>
+            </h4>
           </a>
+        </div>
+        <div class="home-tab with-arrow">
           <a href="javascript:;" @click="goto(`/users`)">
-            <div class="home-tab with-arrow">
-              <img
-                src="https://cgt-dev-ft.microsysx.com/images/v2/icons/Group%20644.svg" width="120" height="100"
-              />
-              <h4 class="heading-home">
-                {{translation.Manag_1_455}} <strong class="strong">{{translation['Users_1_504']}} </strong>
-              </h4>
-            </div>
+            <img
+              src="https://cgt-dev-ft.microsysx.com/images/v2/icons/Group%20644.svg"
+              width="120"
+              height="100"
+            />
+            <h4 class="heading-home">
+              {{ translation.Manag_1_455 }}
+              <strong class="strong">{{ translation['Users_1_504'] }} </strong>
+            </h4>
           </a>
-        </a-col>
-        <!-- #end left side tabs -->
-        <a-col :span="1"></a-col>
-        <a-col :span="11" class="mtminus-6">
-          <treatment-stats />
-        </a-col>
+        </div>
+      </a-col>
+      <!-- #end left side tabs -->
+      <a-col :span="4"></a-col>
+
+      <a-col :md="10" class="mtminus-6">
+        <treatment-stats />
+      </a-col>
     </a-card>
   </div>
 </template>
@@ -55,7 +70,7 @@ export default {
     return {}
   },
   mounted() {},
-  computed:{
+  computed: {
     translation() {
       return this.$store.getters.getTranslation
     },
