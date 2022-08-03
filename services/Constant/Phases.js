888b5c996b30e6cb90678425a08f25bee125ee03
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-08-04 02:35:08 +0300

    fix: minor fixing

--- services/Constant/Phases.js
+++ services/Constant/Phases.js
@@ -111,7 +111,7 @@ export const MANUFACTURER_TREATMENT_PENDING_PHASES = [
         id: 1,
         name: 'Inbound Shipment',
         alias: 'INBOUND_SHIPMENT',
-        phaseId: 8,
+        phaseId: 6,
         enablePageId: 5,
         url_slug: '/manufacturer/treatments/process/',
         url_type: URL_TYPE.patient,
