129dbbc6c72fcce08d4ff9e52bb46c601455aa06
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-08-30 02:32:15 +0300

    fix: so manyy bugs thoushand of bugs

--- components/treatment/enrollment/screening/index.vue
+++ components/treatment/enrollment/screening/index.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>    
+  <div>
     <Create
-      v-if="isScreeningCompleted()"
+      v-if="!isCreated"
       :treatment="treatment"
       @getNextTab="getNextTab"
       @getTreatment="getTreatment"
     />
-    <CollectedList :loading="loading" v-else :categories="categories" />
+    <CollectedList v-else :categories="categories" />
   </div>
 </template>
 <script>
 import Create from '~/components/treatment/enrollment/screening/Create'
 import CollectedList from '~/components/treatment/enrollment/screening/CollectedList'
 import ScreeningCategoryServices from '~/services/API/ScreeningCategoryServices'
-import TreatmentServices from '~/services/API/TreatmentServices'
+// import TreatmentServices from '~/services/API/TreatmentServices'
 export default {
   components: { Create, CollectedList },
   props: {
@@ -23,60 +23,38 @@ export default {
     },
   },
   data() {
-    return { categories: null, isCreated: null, getTreatmentByParamId: {}, treatmentParamId : {}, loading : false }
+    return {
+      categories: null,
+      isCreated: null,
+      getTreatmentByParamId: {},
+      treatmentParamId: {},
+      loading: false,
+    }
   },
   mounted() {
     this.isScreeningCompleted()
   },
   methods: {
     isScreeningCompleted() {
-      debugger
       this.treatmentParamId = this.$route.query.treatment_id
+
       if (this.treatment.screeningStatus) {
         this.isCreated = true
         this.fetchTreatmentScreening(this.treatment)
       }
-      this.fetch(this.treatmentParamId)
-      if(this.treatmentParamId !== null && this.getTreatmentByParamId.screeningStatus === true)
-      {
-        this.isCreated = true
-        this.fetchTreatmentScreening(this.getTreatmentByParamId)
-      }
-      if(this.isCreated === true)
-      {
-        this.loading = true
-         return true
-      }
-      else
-      {
-        return false
-      }
     },
-    fetch(treatmentParamId) {
-      debugger
-      TreatmentServices.detail(treatmentParamId)
-        .then((response) => {
-          this.getTreatmentByParamId = response.data
-          console.log(this.getTreatmentByParamId)
-        })
-        .catch(this.error)
-        .finally(() => {
-          this.loading = false
-        })
-            },
     fetchTreatmentScreening(treatment) {
       ScreeningCategoryServices.getByTreatmentId(treatment.id)
-      .then((response) => {
+        .then((response) => {
           this.categories = response.data
         })
-      .catch(this.error)
+        .catch(this.error)
         .finally(() => (this.loading = false))
     },
     getNextTab(data) {
       this.$emit('getNextTab', data)
     },
     getTreatment(data) {
-      debugger
       this.$emit('getTreatment', data)
     },
   },
