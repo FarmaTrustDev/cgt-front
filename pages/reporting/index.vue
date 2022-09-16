369adcc4f22ab9e334c401ddee736a28d38b2817
Maaz Khan <maazkghori10@gmail.com>
2022-09-17 01:55:28 +0500

    fix: minor status issue in report page

--- pages/reporting/index.vue
+++ pages/reporting/index.vue
@@ -7,16 +7,16 @@
     class="container page-search-input-container"
   >
     <template slot="headerMenus">
-          <a-input
-            ref="userNameInput"
-            :placeholder="translation.searc_1_488"
-            class="page-search-input"
-            @change="searchLabel"
-          >
-            <a-icon slot="prefix" type="search" />
-          </a-input>
+      <a-input
+        ref="userNameInput"
+        :placeholder="translation.searc_1_488"
+        class="page-search-input"
+        @change="searchLabel"
+      >
+        <a-icon slot="prefix" type="search" />
+      </a-input>
     </template>
-<template slot="content">
+    <template slot="content">
       <a-row class="">
         <a-table
           :columns="columns"
@@ -59,7 +59,7 @@ export default {
         {
           id: 'DAC517847',
           sample: 'Adaptimmune',
-          name: 'Stephen Jones',
+          name: 'Chris Smith',
           date: '15/08/2022',
           status: 'Saved',
           doc: 'Form',
@@ -75,7 +75,7 @@ export default {
         {
           id: 'DAC79798',
           sample: 'Novartis',
-          name: 'Stephen Jones',
+          name: 'Oliver Jack',
           date: '20/08/2022',
           status: 'Saved',
           doc: 'Form',
@@ -83,7 +83,7 @@ export default {
         {
           id: 'DAC37790',
           sample: 'Adaptimmune',
-          name: 'Stephen Jones',
+          name: 'Jack Connor',
           date: '20/08/2022',
           status: 'Saved',
           doc: 'Form',
@@ -114,7 +114,7 @@ export default {
           title: 'Status',
           dataIndex: 'status',
           key: 'status',
-          class: 'status-sample'
+          class: 'status-sample',
         },
         {
           title: 'Document',
