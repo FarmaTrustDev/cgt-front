ff67bb05feffc254cda6d0e450b6da26a05ba23e
zulqrnainsial <zulqrnain@microsysx.com>
2023-10-18 17:28:31 +0500

    fix: minor_fixes

--- pages/inventory/treatment/index.vue
+++ pages/inventory/treatment/index.vue
@@ -306,7 +306,7 @@ export const customDisplayData = [
   },
   {
     title: '',
-    value: moment(_getFutureMomentStandardFormatted(1,'day')).format("DD/MM/YYYY"),
+    value: moment(_getFutureMomentStandardFormatted(-2,'day')).format("DD/MM/YYYY"),
     key: 2,
     url: '',
   },
@@ -324,7 +324,7 @@ export const customDisplayData = [
   },
   {
     title: '',
-    value: moment(_getFutureMomentStandardFormatted()).format("DD/MM/YYYY"),
+    value: moment(_getFutureMomentStandardFormatted(-1,'day')).format("DD/MM/YYYY"),
     key: 5,
     url: '',
   },
