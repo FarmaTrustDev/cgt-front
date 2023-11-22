9f8dea8169a43a9393391fa05e06bb2f8f264d4d
MyHoemRepository <hassan@microsysx.com>
2023-11-22 19:02:26 +0500

    feat: process_task

---
+++ services/API/SmartLabTasksServices.js
@@ -0,0 +1,36 @@
+import request from '../request'
+import { BASE_API } from '../Constant'
+
+const baseApi = `${BASE_API}/SmartLabTasks`
+
+function create(data){
+    return request({
+        url: `${baseApi}`,
+        method : 'POST',
+        data,
+    })
+}
+
+function get(){
+    return request({
+        url : `${baseApi}`,
+        method : 'GET'
+    })
+}
+
+function getById(id){
+    return request({
+        url : `${baseApi}/${id}`,
+        method : 'GET'
+    })
+}
+
+
+
+const SmartLabTasksServices = {
+    create,
+    get,
+    getById,
+}
+
+export default SmartLabTasksServices
