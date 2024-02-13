8b24db3c777136de5c5c298c8caea565719c0279
MyHoemRepository <hassan@microsysx.com>
2024-02-13 16:50:30 +0500

    feat: fixed

--- components/treatment/enrollment/scheduling/index.vue
+++ components/treatment/enrollment/scheduling/index.vue
@@ -122,7 +122,7 @@
             <a-row class="row-height"><a-col :span="10"><strong>Container Date:</strong></a-col><a-col :span="14">{{ _getFormatMoment(getMomentByStandardFormat(collectionDate)).format('DD MMMM YYYY hh:mm') }}</a-col></a-row>
           </a-col>
           <a-col :span="9" >
-              <a-col v-for="user in filteredUsers" :key="user.id" ><img class="img-responsive" :src="getImageUrl(getImageFineURL(user.userProfileImageUrl))" width="40" height="30" style="margin-right:15px" />{{ user.name }}</a-col>
+              <a-col v-for="user in filteredUsers" :key="user.id" style="margin-top: 7px;"><img class="img-responsive" :src="getImageUrl(getImageFineURL(user.userProfileImageUrl))" width="40" height="30" style="margin-right:15px" />{{ user.name }}</a-col>
           </a-col>
         </a-row>
         <center>
