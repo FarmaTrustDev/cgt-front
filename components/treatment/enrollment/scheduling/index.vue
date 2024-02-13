b0d17e3989f88cbf2ceee55ead99a32985e44af1
MyHoemRepository <hassan@microsysx.com>
2024-02-13 16:11:51 +0500

    feat: size_allign

--- components/treatment/enrollment/scheduling/index.vue
+++ components/treatment/enrollment/scheduling/index.vue
@@ -111,7 +111,7 @@
         <h2><strong>Appointment Summary</strong></h2>
         <h3 style="color: #1943AE;"><strong>{{patientPUID}} - {{patientName}}</strong></h3>
       </center>
-        <a-row>
+        <a-row style="display: flex; align-items: center;">
           <a-col :span="15">
             <a-row class="row-height"><a-col :span="10"><strong>Collection Date: </strong></a-col><a-col :span="14">{{ _getFormatMoment(getMomentByStandardFormat(collectionDate)).format('DD MMMM YYYY') }}</a-col></a-row>
             <a-row class="row-height"><a-col :span="10"><strong>Treatment Date: </strong></a-col><a-col :span="14">{{ _getFormatMoment(getMomentByStandardFormat(dated)).format('DD MMMM YYYY') }}</a-col></a-row>
@@ -121,8 +121,8 @@
             <a-row class="row-height"><a-col :span="10"><strong>Partner:</strong></a-col><a-col :span="14">{{ partner }}</a-col></a-row>
             <a-row class="row-height"><a-col :span="10"><strong>Container Date:</strong></a-col><a-col :span="14">{{ _getFormatMoment(getMomentByStandardFormat(collectionDate)).format('DD MMMM YYYY hh:mm') }}</a-col></a-row>
           </a-col>
-          <a-col :span="9">
-              <a-col v-for="user in filteredUsers" :key="user.id"><img class="img-responsive" :src="getImageUrl(getImageFineURL(user.userProfileImageUrl))" width="40" height="30" style="margin-right:15px" />{{ user.name }}</a-col>
+          <a-col :span="9" style="display: flex; align-items: center; justify-content: center;" >
+              <a-col v-for="user in filteredUsers" :key="user.id" ><img class="img-responsive" :src="getImageUrl(getImageFineURL(user.userProfileImageUrl))" width="40" height="30" style="margin-right:15px" />{{ user.name }}</a-col>
           </a-col>
         </a-row>
         <center>
