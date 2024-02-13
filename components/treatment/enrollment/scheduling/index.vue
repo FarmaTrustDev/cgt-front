d74242728d9cb951296cd00505e6030dc84f945f
MyHoemRepository <hassan@microsysx.com>
2024-02-13 16:22:36 +0500

    feat: size_fix

--- components/treatment/enrollment/scheduling/index.vue
+++ components/treatment/enrollment/scheduling/index.vue
@@ -121,7 +121,7 @@
             <a-row class="row-height"><a-col :span="10"><strong>Partner:</strong></a-col><a-col :span="14">{{ partner }}</a-col></a-row>
             <a-row class="row-height"><a-col :span="10"><strong>Container Date:</strong></a-col><a-col :span="14">{{ _getFormatMoment(getMomentByStandardFormat(collectionDate)).format('DD MMMM YYYY hh:mm') }}</a-col></a-row>
           </a-col>
-          <a-col :span="9" style="display: flex; align-items: center; justify-content: center;" >
+          <a-col :span="9" >
               <a-col v-for="user in filteredUsers" :key="user.id" ><img class="img-responsive" :src="getImageUrl(getImageFineURL(user.userProfileImageUrl))" width="40" height="30" style="margin-right:15px" />{{ user.name }}</a-col>
           </a-col>
         </a-row>
